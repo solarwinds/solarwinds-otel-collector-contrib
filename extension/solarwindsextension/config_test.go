52e14a4322718c3bfd5b38fd2360cacbd9a9015e
Jiri Janecek <jiri.janecek@solarwinds.com>
2025-05-15 11:56:12 +0200

    Activating full build pipeline for build and test. Linux, mac and Windows.

--- extension/solarwindsextension/config_test.go
+++ extension/solarwindsextension/config_test.go
@@ -40,8 +40,6 @@ func TestConfigUnmarshalFull(t *testing.T) {
 	attributeMap["att1"] = "custom_attribute_value_1"
 	attributeMap["att2"] = "custom_attribute_value_2"
 
-	assert.Fail(t, "die"
-
 	// Verify the values.
 	assert.Equal(t, &internal.Config{
 		DataCenter:          "na-01",
