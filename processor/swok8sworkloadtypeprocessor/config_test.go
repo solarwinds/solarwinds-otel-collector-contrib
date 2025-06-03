bbad79565994b0f928dac46c3ab993ff35535eac
Peter Stranak <37192399+pstranak-sw@users.noreply.github.com>
2025-06-03 08:07:00 +0200

    feature: #20 `swoworkloadtype` processor improvements (#16)
    * Do not overwrite the workload type attribute if it's already set
    
    * Support multiple contexts for attributes
    
    * Refactor tests
    
    * Support searching for workload type by DNS address
    
    * Support searching for workload type by IP address

--- processor/swok8sworkloadtypeprocessor/config_test.go
+++ processor/swok8sworkloadtypeprocessor/config_test.go
@@ -23,7 +23,6 @@ import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/confmap/xconfmap"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/solarwinds/solarwinds-otel-collector-contrib/internal/k8sconfig"
 	"github.com/solarwinds/solarwinds-otel-collector-contrib/processor/swok8sworkloadtypeprocessor/internal/metadata"
@@ -41,6 +40,9 @@ func TestInvalidConfig(t *testing.T) {
 			id: component.NewIDWithName(metadata.Type, "missing_expected_types"),
 		},
 		{
+			id: component.NewIDWithName(metadata.Type, "missing_workload_type"),
+		},
+		{
 			id: component.NewIDWithName(metadata.Type, "invalid_expected_types"),
 		},
 		{
@@ -50,12 +52,27 @@ func TestInvalidConfig(t *testing.T) {
 			id: component.NewIDWithName(metadata.Type, "missing_workload_mappings"),
 		},
 		{
+			id: component.NewIDWithName(metadata.Type, "inconsistent_attribute_context_when_using_name_attribute"),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "inconsistent_attribute_context_when_using_address_attribute"),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "using_both_name_and_addr_attributes"),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "unsupported_workload_type_when_using_address_attribute"),
+		},
+		{
+			id: component.NewIDWithName(metadata.Type, "using_neither_name_nor_addr_attributes"),
+		},
+		{
 			id: component.NewIDWithName(metadata.Type, "valid_config"),
 			expected: &Config{
 				APIConfig: k8sconfig.APIConfig{
 					AuthType: k8sconfig.AuthTypeServiceAccount,
 				},
-				WorkloadMappings: []K8sWorkloadMappingConfig{
+				WorkloadMappings: []*K8sWorkloadMappingConfig{
 					{
 						NameAttr:         "source_workload",
 						NamespaceAttr:    "source_workload_namespace",
@@ -68,6 +85,12 @@ func TestInvalidConfig(t *testing.T) {
 						WorkloadTypeAttr: "dest_workload_type",
 						ExpectedTypes:    []string{"services", "pods"},
 					},
+					{
+						AddressAttr:      "dest_address",
+						NamespaceAttr:    "dest_workload_namespace",
+						WorkloadTypeAttr: "dest_workload_type",
+						ExpectedTypes:    []string{"services"},
+					},
 				},
 				WatchSyncPeriod: time.Minute * 5,
 			},
@@ -78,34 +101,11 @@ func TestInvalidConfig(t *testing.T) {
 		t.Run(tt.id.String(), func(t *testing.T) {
 			mock, reset := MockKubeClient()
 			defer reset()
-			mock.MockedServerPreferredResources = []*metav1.APIResourceList{
-				{
-					GroupVersion: "v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "pods",
-							Kind: "Pod",
-						},
-						{
-							Name: "services",
-							Kind: "Service",
-						},
-						{
-							Name: "withinvalidkinds",
-							Kind: "",
-						},
-					},
-				},
-				{
-					GroupVersion: "apps/v1",
-					APIResources: []metav1.APIResource{
-						{
-							Name: "deployments",
-							Kind: "Deployment",
-						},
-					},
-				},
-			}
+
+			mock.MockServerPreferredResources("v1", "pods", "Pod")
+			mock.MockServerPreferredResources("v1", "services", "Service")
+			mock.MockServerPreferredResources("apps/v1", "deployments", "Deployment")
+			mock.MockServerPreferredResources("v1", "withinvalidkinds", "")
 
 			cm, err := confmaptest.LoadConf(filepath.Join("testdata", "config.yaml"))
 			require.NoError(t, err)
@@ -124,6 +124,236 @@ func TestInvalidConfig(t *testing.T) {
 				require.NoError(t, xconfmap.Validate(cfg))
 				require.EqualExportedValues(t, tt.expected, cfg, "User-configurable fields should be parsed correctly")
 			}
+		})
+	}
+}
+
+func TestMappingContextInConfig(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		id       string
+		expected K8sWorkloadMappingConfig
+		data     K8sWorkloadMappingConfig
+	}{
+		{
+			id: "default_datapoint_context",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          DataPointContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+			},
+		},
+		{
+			id: "default_datapoint_context_no_namespace",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          DataPointContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+			},
+		},
+		{
+			id: "default_datapoint_context_unrelated_prefix",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "something.source_workload",
+				NamespaceAttr:    "something.source_workload_namespace",
+				WorkloadTypeAttr: "something.source_workload_type",
+				context:          DataPointContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "something.source_workload",
+				NamespaceAttr:    "something.source_workload_namespace",
+				WorkloadTypeAttr: "something.source_workload_type",
+			},
+		},
+		{
+			id: "datapoint_context",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          DataPointContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "datapoint.source_workload",
+				NamespaceAttr:    "datapoint.source_workload_namespace",
+				WorkloadTypeAttr: "datapoint.source_workload_type",
+			},
+		},
+		{
+			id: "datapoint_context_no_namespace",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          DataPointContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "datapoint.source_workload",
+				WorkloadTypeAttr: "datapoint.source_workload_type",
+			},
+		},
+		{
+			id: "metric_context",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          MetricContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "metric.source_workload",
+				NamespaceAttr:    "metric.source_workload_namespace",
+				WorkloadTypeAttr: "metric.source_workload_type",
+			},
+		},
+		{
+			id: "metric_context_no_namespace",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          MetricContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "metric.source_workload",
+				WorkloadTypeAttr: "metric.source_workload_type",
+			},
+		},
+		{
+			id: "scope_context",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          ScopeContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "scope.source_workload",
+				NamespaceAttr:    "scope.source_workload_namespace",
+				WorkloadTypeAttr: "scope.source_workload_type",
+			},
+		},
+		{
+			id: "scope_context_no_namespace",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          ScopeContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "scope.source_workload",
+				WorkloadTypeAttr: "scope.source_workload_type",
+			},
+		},
+		{
+			id: "resource_context",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          ResourceContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "resource.source_workload",
+				NamespaceAttr:    "resource.source_workload_namespace",
+				WorkloadTypeAttr: "resource.source_workload_type",
+			},
+		},
+		{
+			id: "resource_context_no_namespace",
+			expected: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				WorkloadTypeAttr: "source_workload_type",
+				context:          ResourceContext,
+			},
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "resource.source_workload",
+				WorkloadTypeAttr: "resource.source_workload_type",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id, func(t *testing.T) {
+
+			// mock expected types for the test
+			tt.expected.ExpectedTypes = []string{"deployments"}
+			tt.data.ExpectedTypes = []string{"deployments"}
+			mappedExpectedTypes := map[string]groupVersionResourceKind{
+				"deployments": {
+					kind: "Deployment",
+				}}
+
+			err := tt.data.validate(nil, mappedExpectedTypes)
+
+			require.NoError(t, err)
+			require.Equal(t, tt.expected, tt.data, "Initialized config should be equal to expected config")
+		})
+	}
+}
+
+func TestMappingMismatchedContextInConfig(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		id   string
+		data K8sWorkloadMappingConfig
+	}{
+		{
+			id: "default_and_datapoint",
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "source_workload",
+				NamespaceAttr:    "source_workload_namespace",
+				WorkloadTypeAttr: "datapoint.source_workload_type",
+			},
+		},
+		{
+			id: "datapoint_and_scope",
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "scope.source_workload",
+				WorkloadTypeAttr: "datapoint.source_workload_type",
+			},
+		},
+		{
+			id: "datapoint_and_resource",
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "resource.source_workload",
+				NamespaceAttr:    "datapoint.source_workload_namespace",
+				WorkloadTypeAttr: "resource.source_workload_type",
+			},
+		},
+		{
+			id: "metric_and_resource",
+			data: K8sWorkloadMappingConfig{
+				NameAttr:         "resource.source_workload",
+				NamespaceAttr:    "metric.source_workload_namespace",
+				WorkloadTypeAttr: "resource.source_workload_type",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.id, func(t *testing.T) {
+
+			// mock expected types for the test
+			tt.data.ExpectedTypes = []string{"deployments"}
+			mappedExpectedTypes := map[string]groupVersionResourceKind{
+				"deployments": {
+					kind: "Deployment",
+				}}
+
+			err := tt.data.validate(nil, mappedExpectedTypes)
+
+			require.EqualError(t, err, "inconsistent context in workload mapping")
 		})
 	}
 }
