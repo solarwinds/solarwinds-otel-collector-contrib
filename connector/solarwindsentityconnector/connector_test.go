677c497ef981ac939c07a96ea939ec89c55547d8
akumetsu183 <milan.kuba@solarwinds.com>
2025-06-09 10:59:02 +0200

    tweaks

--- connector/solarwindsentityconnector/connector_test.go
+++ connector/solarwindsentityconnector/connector_test.go
@@ -46,8 +46,7 @@ func TestLogsToLogs(t *testing.T) {
 			folder: "entity/01-happy-path",
 		},
 		{
-			// Checks creation of 1 Kubernetes entity with valid complex condition
-			name:   "when log for entity has valid condition, log event is sent",
+			name:   "when log for entity has valid complex condition, log event is sent",
 			folder: "entity/02-valid-condition",
 		},
 		{
@@ -58,8 +57,7 @@ func TestLogsToLogs(t *testing.T) {
 		},
 		{
 			// Attributes for schema.entities are sufficient for entity creation, but
-			// condition in schema.event.entities is no satisfied.
-			// No event should be sent.
+			// condition in schema.event.entities is not satisfied.
 			name:   "when log for entity has no valid condition, no log event is sent",
 			folder: "entity/04-no-valid-condition",
 		},
@@ -73,24 +71,23 @@ func TestLogsToLogs(t *testing.T) {
 		},
 		{
 			// Testing that more complex conditions get evaluated correctly.
-			// Checks that events for entities and relationships are still sent.
+			// Checks that events for entities and the relationship are still sent.
 			name:   "when same type relationship has valid advanced condition, log event is sent",
 			folder: "relationship/same-type-relationship/02-advanced-conditions",
 		},
 		{
-			// Tests that when two entities share an attribute key, and also value for some required attribute,
-			// event for entities and their relationship is still detected and stent.
+			// Tests that when two entities share an attribute key and value for some required attribute,
+			// the events for entities and their relationship are still detected and sent.
 			name:   "when relationship for same type and having common id attributes is inferred log event is sent",
 			folder: "relationship/same-type-relationship/03-common-atr",
 		},
 		{
-			// Checks that if one of the two entities is not created, their relationship is not sent.
+			// Checks that if one of the attributes for the relationship is not set, the relationship is not sent, but is for entities.
 			name:   "when relationship for same type is not inferred no log is sent",
 			folder: "relationship/same-type-relationship/04-no-match",
 		},
 		{
-			// Checks that when additional attributes are set on the relationship and that
-			// they are sent with the relationship.
+			// Checks that when additional attributes are set on the relationship, they are sent with the relationship.
 			name:   "when log for same type relationship, log event is sent with relationship attributes",
 			folder: "relationship/same-type-relationship/05-res-atr",
 		},
@@ -102,8 +99,8 @@ func TestLogsToLogs(t *testing.T) {
 			folder: "relationship/different-types-relationship/01-with-prefixes",
 		},
 		{
-			// Checks that different type relationship works without prefixes.
-			// Since there are no prefixes, the attributes match entities and the relationship so 3 log records are sent.
+			// Checks that different type relationship works without prefixes configuration.
+			// Since there are no prefixes, the attributes match the entities and their relationship, so 3 events are sent.
 			name:   "different type relationship works without prefixes",
 			folder: "relationship/different-types-relationship/02-without-prefixes",
 		},
@@ -130,7 +127,7 @@ func TestLogsToLogs(t *testing.T) {
 			folder: "relationship/different-types-relationship/06-valid-condition",
 		},
 		{
-			// Relationship should be sent with the extra attribute, and also 2 entity log events.
+			// Relationship should be sent with the extra attributes, and also 2 entity log events.
 			name:   "when log for different type relationship, log event is sent with relationship attributes",
 			folder: "relationship/different-types-relationship/07-res-atr",
 		},
@@ -187,20 +184,17 @@ func TestMetricsToLogs(t *testing.T) {
 			folder: "entity/01-happy-path",
 		},
 		{
-			// Checks creation of 1 Kubernetes entity with valid complex condition
-			name:   "when log for entity has valid condition, log event is sent",
+			name:   "when log for entity has valid complex condition, log event is sent",
 			folder: "entity/02-valid-condition",
 		},
 		{
 			// Input is sending insufficient attributes for entity creation
-			// No event should be sent.
 			name:   "when entity is not inferred no log is sent",
 			folder: "entity/03-no-match",
 		},
 		{
 			// Attributes for schema.entities are sufficient for entity creation, but
-			// condition in schema.event.entities is no satisfied.
-			// No event should be sent.
+			// condition in schema.event.entities is not satisfied.
 			name:   "when log for entity has no valid condition, no log event is sent",
 			folder: "entity/04-no-valid-condition",
 		},
@@ -214,25 +208,24 @@ func TestMetricsToLogs(t *testing.T) {
 		},
 		{
 			// Testing that more complex conditions get evaluated correctly.
-			// Checks that events for entities and relationships are still sent.
+			// Checks that events for entities and relationship are still sent.
 			name:   "when same type relationship has valid advanced condition, log event is sent",
 			folder: "relationship/same-type-relationship/02-advanced-conditions",
 		},
 		{
-			// Tests that when two entities share an attribute key, and also value for some required attribute,
-			// event for entities and their relationship is still detected and stent.
+			// Tests that when two entities share an attribute key and value for some required attribute,
+			// the events for entities and their relationship are still detected and sent.
 			name:   "when relationship for same type and having common id attributes is inferred log event is sent",
 			folder: "relationship/same-type-relationship/03-common-atr",
 		},
 		{
-			// Checks that if one of the two entities is not created, their relationship is not sent.
+			// Checks that if one of the attributes for the relationship is not set, the relationship is not sent, but is for entities.
 			name:   "when relationship for same type is not inferred no log is sent",
 			folder: "relationship/same-type-relationship/04-no-match",
 		},
 		{
 
-			// Checks that when additional attributes are set on the relationship, and that
-			// they are sent with the relationship.
+			// Checks that when additional attributes are set on the relationship, they are sent with the relationship.
 			name:   "when log for same type relationship, log event is sent with relationship attributes",
 			folder: "relationship/same-type-relationship/05-res-atr",
 		},
@@ -244,8 +237,8 @@ func TestMetricsToLogs(t *testing.T) {
 			folder: "relationship/different-types-relationship/01-with-prefixes",
 		},
 		{
-			// Checks that different type relationship works without prefixes.
-			// Since there are no prefixes, the attributes match entities and the relationship so 3 log records are sent.
+			// Checks that different type relationship works without prefixes configuration.
+			// Since there are no prefixes, the attributes match the entities and their relationship, so 3 events are sent.
 			name:   "different type relationship works without prefixes",
 			folder: "relationship/different-types-relationship/02-without-prefixes",
 		},
@@ -262,7 +255,7 @@ func TestMetricsToLogs(t *testing.T) {
 		},
 		{
 			// Checks that when there is an extra attribute, that has nothing to do with entities or relationship,
-			// relationship and entities are still sent.
+			// relationship and entities are still sent if they have what is needed.
 			name:   "when log for different type relationship has redundant attributes, log event is sent",
 			folder: "relationship/different-types-relationship/05-redundant-atr",
 		},
@@ -272,7 +265,7 @@ func TestMetricsToLogs(t *testing.T) {
 			folder: "relationship/different-types-relationship/06-valid-condition",
 		},
 		{
-			// Relationship should be sent with the extra attribute, and also 2 entity log events.
+			// Relationship should be sent with the extra attributes, and also 2 entity log events.
 			name:   "when log for different type relationship, log event is sent with relationship attributes",
 			folder: "relationship/different-types-relationship/07-res-atr",
 		},
