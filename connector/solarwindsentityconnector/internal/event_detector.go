c82115e642dd0f2a2703b3520e3a29a2beae8255
janatrelakova <jana.trelakova@solarwinds.com>
2025-07-14 20:56:15 +0200

    Add docs to event detector

--- connector/solarwindsentityconnector/internal/event_detector.go
+++ connector/solarwindsentityconnector/internal/event_detector.go
@@ -62,40 +62,59 @@ func (e *EventDetector) DetectMetric(ctx context.Context, resourceAttrs Attribut
 
 }
 
+// collectEvents processes the attributes and configured events to create a list of detected events.
+// First, it gathers entity events and relationship events with their associated entities.
+// Then, it validates the relationship entities against the configured entity events to ensure that only
+// valid and unique entity events are included.
 func (e *EventDetector) collectEvents(
 	attrs Attributes,
 	configuredEvents []*config.EntityEvent,
 	configuredRelationships []*config.RelationshipEvent,
 ) ([]Event, error) {
+	entityEvents := e.getEntities(attrs, configuredEvents)
 	relationshipEvents, relationshipEntityEvents := e.getRelationships(attrs, configuredRelationships)
-	validRelationshipEntities := e.validateEntityEvents(configuredEvents, relationshipEntityEvents)
-	entityEvents := e.getEntities(attrs, configuredEvents, validRelationshipEntities)
+	validRelationshipEntities := e.validateEntityEvents(configuredEvents, entityEvents, relationshipEntityEvents)
 
-	allEvents := append(entityEvents)
-	for _, entity := range validRelationshipEntities {
+	allEvents := append(relationshipEvents)
+	for _, entity := range entityEvents {
 		allEvents = append(allEvents, entity)
 	}
-	allEvents = append(allEvents, relationshipEvents...)
+	allEvents = append(allEvents, validRelationshipEntities...)
 	return allEvents, nil
 }
 
-func (e *EventDetector) validateEntityEvents(validEvents []*config.EntityEvent, actualEvents map[string]Entity) map[string]Entity {
-	events := make(map[string]Entity)
-	for entityHash, actualEvent := range actualEvents {
-		for _, event := range validEvents {
+// validateEntityEvents checks if the entities created from relationship events are not duplicates
+// of those created from entity events. It filters out entities that already exist in the configured entity events.
+// It returns a slice of events that are compared to configured entity events to ensure that only valid events are returned.
+func (e *EventDetector) validateEntityEvents(
+	configuredEntityEvents []*config.EntityEvent,
+	alreadyExistingEntities,
+	relationshipEntities map[string]Entity,
+) []Event {
+	events := make([]Event, 0)
+	for entityHash, actualEvent := range relationshipEntities {
+		if _, exists := alreadyExistingEntities[entityHash]; exists {
+			// If the entity already exists (created from entity event, not from relationship event), we can skip it.
+			continue
+		}
+
+		// If entity was created from relationship, we need to check whether it can be sent in the resulting log.
+		// We do not want to send entity event if it was not configured in the entity events.
+		for _, event := range configuredEntityEvents {
+			// If the event type matches, we know that we can infer the entity
+			// because conditions were met.
 			if event.Type == actualEvent.Type {
-				// If the event type matches, we know that we can infer the entity
-				// because conditions were met.
 				actualEvent.Action = event.Action
-				events[entityHash] = actualEvent
+				events = append(events, actualEvent)
 			}
 		}
 	}
 	return events
 }
 
-func (e *EventDetector) getEntities(attrs Attributes, entityEvents []*config.EntityEvent, relationshipEntities map[string]Entity) []Event {
-	detectedEntityEvents := make([]Event, 0, len(entityEvents))
+// getEntities creates entity update events based on the configured entity events.
+func (e *EventDetector) getEntities(attrs Attributes, entityEvents []*config.EntityEvent) map[string]Entity {
+	detectedEntityEvents := make(map[string]Entity)
 	for _, entityEvent := range entityEvents {
 		event, err := e.attributeMapper.getEntity(entityEvent.Type, attrs)
 		if err != nil {
@@ -103,16 +122,23 @@ func (e *EventDetector) getEntities(attrs Attributes, entityEvents []*config.Ent
 			continue
 		}
 
+		// Build hash for later comparison between entities created
+		// from relationship and entity events.
 		eventHash, err := buildEntityHash(*event)
-		_, alreadyExists := relationshipEntities[eventHash]
-		if !alreadyExists {
-			event.Action = entityEvent.Action
-			detectedEntityEvents = append(detectedEntityEvents, *event)
+		if err != nil {
+			e.logger.Debug("failed to build entity hash", zap.Error(err))
+			continue
 		}
+		event.Action = entityEvent.Action
+		detectedEntityEvents[eventHash] = *event
 	}
 	return detectedEntityEvents
 }
 
+// getRelationships creates relationship events based on the configured relationships.
+// It also builds a map of entities tied to that relationship - source and destination.
+// The map is used to compare entities created from relationship events with entities created from entity events,
+// so duplicated can be filtered out in case of unprefixed attributes.
 func (e *EventDetector) getRelationships(attrs Attributes, configuredRelationships []*config.RelationshipEvent) ([]Event, map[string]Entity) {
 	relationshipEvents := make([]Event, 0, len(configuredRelationships))
 	relationshipEntityEvents := make(map[string]Entity)
@@ -123,6 +149,7 @@ func (e *EventDetector) getRelationships(attrs Attributes, configuredRelationshi
 			continue
 		}
 
+		// Prepare entity hashes for later comparison with entities created from entity events.
 		srcHash, err := buildEntityHash(*sourceEntity)
 		relationshipEntityEvents[srcHash] = *sourceEntity
 
