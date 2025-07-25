5740d13d7659e6ec1919a1185b78715b8f114963
Kristian Dobes <Kristian.Dobes@solarwinds.com>
2025-07-25 15:48:25 +0200

    remove unused method

--- receiver/swohostmetricsreceiver/internal/cli/process_command.go
+++ receiver/swohostmetricsreceiver/internal/cli/process_command.go
@@ -15,42 +15,10 @@
 package cli
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"go.uber.org/zap"
 )
-
-// ProcessJSONCommand executes the command that outputs JSON data within the CommandLineExecutor and returns that data unmarshalled
-// Returns an error instead of data when command or unmarshalling fails (also logs this error with additional info packed inside).
-func ProcessJSONCommand[TResult any](
-	cle CommandLineExecutor,
-	command string,
-	logger *zap.Logger,
-) (TResult, error) {
-	// actual scraping
-	stdout, stderr, err := cle.ExecuteCommand(command)
-	if err != nil || stderr != "" {
-		logExecutionError(command, stdout, stderr, err, logger)
-		if err == nil {
-			err = fmt.Errorf("%s", stderr)
-		}
-		return *new(TResult), err
-	}
-
-	var result TResult
-	err = json.Unmarshal([]byte(stdout), &result)
-	if err != nil {
-		zap.L().Error(
-			fmt.Sprintf("Unmarshaling result of command %s failed", command),
-			zap.Error(err),
-		)
-		return *new(TResult), err
-	}
-
-	zap.L().Debug(fmt.Sprintf("Command %s succeeded with result %+v", command, result))
-	return result, nil
-}
 
 // ProcessCommand executes passed command with the help of CommandLineExecutor.
 // Returns output from stdout and error indicator.
