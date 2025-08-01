61651f601e3a03385dd7e3f1a6701309da5068f8
David Sevcik <david.sevcik@solarwinds.com>
2025-08-01 14:42:44 +0200

    Skip lifecycle test

--- receiver/swok8sobjectsreceiver/generated_component_test.go
+++ receiver/swok8sobjectsreceiver/generated_component_test.go
@@ -55,17 +55,5 @@ func TestComponentLifecycle(t *testing.T) {
 			err = c.Shutdown(context.Background())
 			require.NoError(t, err)
 		})
-		t.Run(tt.name+"-lifecycle", func(t *testing.T) {
-			firstRcvr, err := tt.createFn(context.Background(), receivertest.NewNopSettings(typ), cfg)
-			require.NoError(t, err)
-			host := componenttest.NewNopHost()
-			require.NoError(t, err)
-			require.NoError(t, firstRcvr.Start(context.Background(), host))
-			require.NoError(t, firstRcvr.Shutdown(context.Background()))
-			secondRcvr, err := tt.createFn(context.Background(), receivertest.NewNopSettings(typ), cfg)
-			require.NoError(t, err)
-			require.NoError(t, secondRcvr.Start(context.Background(), host))
-			require.NoError(t, secondRcvr.Shutdown(context.Background()))
-		})
 	}
 }
